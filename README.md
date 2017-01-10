956ea2320015d50eeb1abeb7974bbbafb6fb2183
Daniel Cheng <dcheng334@gmail.com>
2017-01-10 06:36:59 +0000

    CHG: First commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CrossSensorSystems
 Architectural implementation of low-bandwidth, high quality audio-visual stream processing for the purpose of high-level data analysis.
+
+This python script will run a face detection video analysis server, accessible at localhost:8080/index.html.
+
+Setup:
+Raspberry Pi 3 running Raspbian.
+Install OpenCV and Python 2.
+Run this script by executing "python2.7  opencv_mjpeg_server.py"
+
---
+++ opencv_mjpeg_server.py
@@ -0,0 +1,122 @@
+'''
+	Author: Igor Maculan - n3wtron@gmail.com
+	A Simple mjpg stream http server
+'''
+import cv2
+import PIL as pillow
+from PIL import Image
+import threading
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from SocketServer import ThreadingMixIn
+import StringIO
+import time
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import imutils
+capture=None
+hog=None
+faceCascade=None
+eyeCascade=None
+
+def detectPedestrians(image):
+	image = imutils.resize(image, width=min(320, image.shape[1]))
+ 
+	# detect people in the image
+	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+		padding=(8, 8), scale=1.5)
+ 
+	# apply non-maxima suppression to the bounding boxes using a
+	# fairly large overlap threshold to try to maintain overlapping
+	# boxes that are still people
+	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+ 
+	# draw the final bounding boxes
+	for (xA, yA, xB, yB) in pick:
+		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)	
+		print xA
+
+	return image
+
+def detectFaces(image):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	try:
+		faces = faceCascade.detectMultiScale(gray, 1.3, 5)
+		for (x,y,w,h) in faces:
+			cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
+			roi_gray = gray[y:y+h, x:x+w]
+			roi_color = image[y:y+h, x:x+w]
+			eyes = eyeCascade.detectMultiScale(roi_gray)
+			for (ex,ey,ew,eh) in eyes:
+				cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	except:
+		return image	
+	return image
+
+class CamHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		if self.path.endswith('.mjpg'):
+			self.send_response(200)
+			self.send_header('Content-type','multipart/x-mixed-replace; boundary=--jpgboundary')
+			self.end_headers()
+			while True:
+				try:
+					rc,img = capture.read()
+					if not rc:
+						continue
+					img = detectFaces(img)
+					imgRGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+					jpg = Image.fromarray(imgRGB)
+					tmpFile = StringIO.StringIO()
+					jpg.save(tmpFile,'JPEG')
+					self.wfile.write("--jpgboundary")
+					self.send_header('Content-type','image/jpeg')
+					self.send_header('Content-length',str(tmpFile.len))
+					self.end_headers()
+					jpg.save(self.wfile,'JPEG')
+					time.sleep(0.05)
+				except KeyboardInterrupt:
+					return
+			return
+		if self.path.endswith('.html'):
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write('<html><head></head><body>')
+			self.wfile.write('<img src="http://172.113.170.244:8080/cam.mjpg"/>')
+			self.wfile.write('</body></html>')
+			return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+	"""Handle requests in a separate thread."""
+
+def main():
+	global hog	
+	hog = cv2.HOGDescriptor()
+	hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+	global faceCascade
+	faceCascPath = "opencv-3.1.0/data/haarcascades/haarcascade_frontalface_default.xml"
+	faceCascade = cv2.CascadeClassifier(faceCascPath)
+	global eyeCascade
+	eyeCascPath = "opencv-3.1.0/data/haarcascades/haarcascade_eye.xml"
+	eyeCascade = cv2.CascadeClassifier(eyeCascPath)
+	global capture
+	capture = cv2.VideoCapture(0)
+	capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640); 
+	capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480);
+	capture.set(cv2.CAP_PROP_SATURATION,0.2);
+	global img
+	try:
+		server = ThreadedHTTPServer(('', 8080), CamHandler)
+		print "server started"
+		server.serve_forever()
+	except KeyboardInterrupt:
+		capture.release()
+		server.socket.close()
+
+if __name__ == '__main__':
+	main()
+
