12132774b4ad167a179970cc99f6ef912f25af17
daniel-cheng <dcheng334@gmail.com>
2017-01-23 08:13:17 +0000

    ADD: Adding opencv installation script

---
+++ install-opencv.sh
@@ -0,0 +1,64 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+
+# INSTALL THE DEPENDENCIES
+
+# Build tools:
+sudo apt-get install -y build-essential cmake pkg-config
+
+# GUI (if you want to use GTK instead of Qt, replace 'qt5-default' with 'libgtkglext1-dev' and remove '-DWITH_QT=ON' option in CMake):
+sudo apt-get install -y libgtk2.0-dev
+
+# Media I/O:
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+
+# Video I/O:
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev
+
+# Parallelism and linear algebra libraries:
+sudo apt-get install -y libatlas-base-dev gfortran
+
+# Python:
+sudo apt-get install -y python2.7-dev python3-dev
+wget https://bootstrap.pypa.io/get-pip.py
+
+# Install pip
+sudo python get-pip.py
+sudo pip install virtualenv virtualenvwrapper
+sudo rm -rf ~/.cache/pip get-pip.py
+
+#Create virtualenv for Python 2
+echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.profile
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.profile
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
+source ~/.profile
+mkvirtualenv cv -p python2
+pip install numpy
+
+# INSTALL THE LIBRARY (YOU CAN CHANGE '3.1.0' FOR THE LAST STABLE VERSION)
+
+sudo apt-get install -y unzip wget
+cd ~
+wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
+rm -rf opencv
+unzip opencv.zip
+rm opencv.zip
+wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
+rm -rf opencv_contrib
+unzip opencv_contrib.zip
+rm opencv_contrib.zip
+cd ~/opencv-3.1.0/
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+    -D CMAKE_INSTALL_PREFIX=/usr/local \
+    -D INSTALL_PYTHON_EXAMPLES=ON \
+    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
+    -D BUILD_EXAMPLES=ON ..
+make -j4
+sudo make install
+sudo ldconfig
+
+# Symlink OpenCV packages
+cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
+ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
